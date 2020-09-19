7553894c7d1f521b685ec6c51c9eb5b7c9066fe4
Luqman Shofuleji <luqmanshof@gmail.com>
2020-09-19 11:35:00 +0100

    Postgre, Team Model & Media Files

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100, default='')
+    twitter_link = models.URLField(max_length=100, default='')
+    google_plus_link = models.URLField(max_length=100, default='')
+    linkedin_link = models.URLField(max_length=100, default='')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
