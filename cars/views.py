5512fcaaee5ee5d70eeb035edeccc87ef4cf60c5
Luqman Shofuleji <luqmanshof@gmail.com>
2020-09-27 09:25:09 +0100

    Single car pagination & search

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Car
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
 # Create your views here.
 
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    data = {
+        'cars': paged_cars,
+        # 'cars': cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars/car_detail.html', data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/search.html', data)
